5c6486acb503b55b52576699dc7d70733332351a
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-05-14 01:55:59 -0700

    Add function to display next question

--- quiz.js
+++ quiz.js
@@ -11,3 +11,24 @@ const resultContainer = document.getElementById('result-container');
 const finalScore = document.getElementById('final-score');
 const resultMessage = document.getElementById('result-message');
 const customContent = document.getElementById('custom-content');
+
+function displayQuestion(index) {
+  if (index > 0) {
+    questions[index - 1].style.display = 'none';
+  }
+
+  if (index < questions.length) {
+    questions[index].style.display = 'block';
+    nextBtn.style.display = 'none';
+    responseMessage.textContent = '';
+  } else {
+    showResults(score);
+  }
+}
+
+function nextQuestion() {
+  currentQuestionIndex++;
+  displayQuestion(currentQuestionIndex);
+}
+
+displayQuestion(currentQuestionIndex);

6fce9b772f791592b54c183df907b10cb0961c29
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-05-14 01:56:42 -0700

    Add function to show result message and handle yes no button

--- quiz.js
+++ quiz.js
@@ -26,6 +26,55 @@ function displayQuestion(index) {
   }
 }
 
+function showResults(finalScore, customHTML) {
+  quizContainer.style.display = 'none';
+  resultContainer.style.display = 'block';
+  finalScore.textContent = score;
+
+  if (customHTML) {
+    customContent.innerHTML = customHTML;
+  } else {
+    customContent.innerHTML = '';
+    if (score >= 3) {
+      resultMessage.innerHTML += `
+          <p>Whoa there, speedy shopper! It looks like you're ready to throw down some serious cash. But let's slow down for just a second. Take a deep breath and ask yourself, 'Do I really need this?' We believe in you, so take a moment to reconsider before you make it rain.</p>
+        `;
+    } else {
+      resultMessage.innerHTML += `
+          <p>You're officially allowed to treat yourself guilt-free! Just remember to stay within your budget and think twice before making any big purchases. Happy shopping! 🛍️</p>
+        `;
+    }
+  }
+}
+
+yesButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    currentQuestionIndex++;
+    displayQuestion(currentQuestionIndex);
+  });
+});
+
+noButtons.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    if (index === 0) {
+      const customHTML = `
+        <p>If the item you want to buy is not within your budget, it's likely an impulsive buy. Pause and consider the following exercises:</p>
+        <p>1. Use the 24-Hour Rule: Wait 24 hours before making the purchase. This helps you reassess and ensure it aligns with your financial goals.</p>
+        <p>2. Reflect on Value: Think about the long-term value and impact of the purchase. Is it truly necessary and worth the cost?</p>
+        <p>3. Evaluate Opportunity Costs: Consider what else you could do with the money. Are there more meaningful ways to utilize it?</p>
+        <p>By following these exercises, you'll make more informed and responsible purchasing decisions. Prioritize your financial well-being and stay within your budgetary limits.</p>
+      `;
+
+      showResults(0, customHTML);
+    } else {
+      score++;
+      nextBtn.style.display = 'inline-block';
+      btn.style.display = 'none';
+      yesButtons[index].style.display = 'none';
+    }
+  });
+});
+
 function nextQuestion() {
   currentQuestionIndex++;
   displayQuestion(currentQuestionIndex);
